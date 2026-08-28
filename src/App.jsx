066ab2311445a53043525bc7f7868f3bcf8a0f7a
Alek-Grujic <alek.grujic001@outlook.com>
2026-08-28 03:06:38 +0200

    feat: add page navigation and styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Homepage from "./pages/Homepage.jsx";
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
-import PageNotFound from "./pages/PageNotFount.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,14 +1,18 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
         <li>
-          <NavLink to={"/"}>Home</NavLink>
           <NavLink to={"/pricing"}>Pricing</NavLink>
           <NavLink to={"/product"}>Product</NavLink>
+          <NavLink to={"/login"} className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
