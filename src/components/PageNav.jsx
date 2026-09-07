de6499914fb75bf15ba3b23c3d899316d10d7715
Alek-Grujic <alek.grujic001@outlook.com>
2026-09-07 07:27:50 +0200

    feat: fetch and display cities list

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -9,7 +9,11 @@ function PageNav() {
       <ul>
         <li>
           <NavLink to={"/pricing"}>Pricing</NavLink>
+        </li>
+        <li>
           <NavLink to={"/product"}>Product</NavLink>
+        </li>
+        <li>
           <NavLink to={"/login"} className={styles.ctaLink}>
             Login
           </NavLink>
